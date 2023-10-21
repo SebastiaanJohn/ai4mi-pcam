15cb3f345d413fafee7d787f126b6962471f568b
Sebastiaan Dijkstra <sebastiaandijkstra@gmail.com>
2023-10-21 18:50:37 +0200

    chore(PCAM): Refactor PCAM dataset initialization
    - Refactor the `__init__` method in `PCAMDataset` class to remove unnecessary arguments and attributes.
    - Update the `__len__` method to retrieve dataset length from the data file.
    - Update the `__getitem__` method to return the image and target tensor as an output.

--- src/datasets/PCAM/datamodule.py
+++ src/datasets/PCAM/datamodule.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 
 import lightning.pytorch as pl
-from lightning.pytorch.utilities.types import EVAL_DATALOADERS
 from torch.utils.data import DataLoader
 
 from .dataset import PCAMDataset
@@ -12,22 +11,13 @@ from .dataset import PCAMDataset
 class PCAMDataModule(pl.LightningDataModule):
     """PyTorch Lightning DataModule for PCAM dataset."""
 
-    def __init__(
-        self,
-        data_dir: Path,
-        batch_size: int = 32,
-        num_workers: int = 8,
-        lazy_loading: bool = False,
-        crop_center: bool = False,
-    ) -> None:
+    def __init__(self, data_dir: Path, batch_size: int = 32, num_workers: int = 8, transforms=None) -> None:
         """Initialize the PCAMDataModule."""
         super().__init__()
         self._data_dir = data_dir
         self._batch_size = batch_size
         self._num_workers = num_workers
-        self._lazy_loading = lazy_loading
-        self._transform = None
-        self._crop_center = crop_center
+        self._transform =transforms
         self._num_classes = 1
 
     @property
@@ -42,15 +32,11 @@ class PCAMDataModule(pl.LightningDataModule):
                 data_file_path=self._data_dir / "camelyonpatch_level_2_split_train_x.h5",
                 target_file_path=self._data_dir / "camelyonpatch_level_2_split_train_y.h5",
                 transform=self._transform,
-                lazy_loading=self._lazy_loading,
-                crop_center=self._crop_center,
             )
             self._val_dataset = PCAMDataset(
                 data_file_path=self._data_dir / "camelyonpatch_level_2_split_valid_x.h5",
                 target_file_path=self._data_dir / "camelyonpatch_level_2_split_valid_y.h5",
                 transform=self._transform,
-                lazy_loading=self._lazy_loading,
-                crop_center=self._crop_center,
             )
 
         if stage in {"test", "predict"}:
@@ -58,8 +44,6 @@ class PCAMDataModule(pl.LightningDataModule):
                 data_file_path=self._data_dir / "camelyonpatch_level_2_split_test_x.h5",
                 target_file_path=self._data_dir / "camelyonpatch_level_2_split_test_y.h5",
                 transform=self._transform,
-                lazy_loading=self._lazy_loading,
-                crop_center=self._crop_center,
             )
 
     def train_dataloader(self) -> DataLoader:
@@ -80,7 +64,7 @@ class PCAMDataModule(pl.LightningDataModule):
             self._test_dataset, batch_size=self._batch_size, shuffle=False, num_workers=self._num_workers,
         )
 
-    def predict_dataloader(self) -> EVAL_DATALOADERS:
+    def predict_dataloader(self) -> DataLoader:
         """Return the prediction dataloader."""
         return self.test_dataloader()
 
--- src/datasets/PCAM/dataset.py
+++ src/datasets/PCAM/dataset.py
@@ -8,7 +8,6 @@ import torch
 from PIL import Image
 from PIL.Image import Image as ImageType
 from torch.utils.data import Dataset
-from torchvision import transforms
 
 
 class PCAMDataset(Dataset):
@@ -19,63 +18,35 @@ class PCAMDataset(Dataset):
         data_file_path: Path,
         target_file_path: Path,
         transform: Callable | None = None,
-        lazy_loading: bool = False,
-        crop_center: bool = False,
     ) -> None:
-        """PCAM dataset constructor.
+        """PCAM Datset.
 
         Args:
             data_file_path (Path): Path to the data file.
             target_file_path (Path): Path to the target file.
             transform (Callable | None, optional): Transform to apply to the data.
-            Defaults to None.
-            lazy_loading (bool, optional): Whether to load the data lazily.
-            Defaults to False.
-            crop_center (bool, optional): Whether to crop the center 32x32px region of the image.
-            Defaults to False.
+                Defaults to None.
         """
         self._data_file_path = data_file_path
         self._target_file_path = target_file_path
         self._transform = transform
-        self._lazy_loading = lazy_loading
-        self._crop_center = crop_center
-
-        with h5py.File(self._data_file_path, "r") as f:
-            self._data_length = len(f["x"])
-            if not self._lazy_loading:
-                self._data = f["x"][:]
-
-        with h5py.File(self._target_file_path, "r") as f:
-            target_length = len(f["y"])
-            if not self._lazy_loading:
-                self._target = torch.tensor(f["y"][:], dtype=torch.long).squeeze()
-
-        if self._data_length != target_length:
-            msg = "Mismatch between data and target lengths."
-            raise ValueError(msg)
 
     def __len__(self) -> int:
         """Return the length of the dataset."""
-        return len(self._data) if not self._lazy_loading else self._data_length
+        with h5py.File(self._data_file_path, "r") as f:
+            return len(f["x"])
 
     def __getitem__(self, idx: int) -> tuple[ImageType, torch.Tensor]:
         """Return the item at the given index."""
-        if self._lazy_loading:
-            with h5py.File(self._data_file_path, "r") as f:
-                sample = Image.fromarray(f["x"][idx])
+        with h5py.File(self._data_file_path, "r") as f:
+            image = Image.fromarray(f["x"][idx]).convert("RGB")
 
-            with h5py.File(self._target_file_path, "r") as f:
-                target = torch.tensor(f["y"][idx], dtype=torch.long).squeeze()
-        else:
-            sample = Image.fromarray(self._data[idx])
-            target = self._target[idx]
+        with h5py.File(self._target_file_path, "r") as f:
+            target = torch.tensor(f["y"][idx, 0, 0, 0], dtype=torch.float32)
 
-        if self._crop_center:
-            # Crop the center 32x32px region
-            sample = sample.crop((16, 16, 48, 48))
+        if self._transform:
+            image = self._transform(image)
 
-        sample = self._transform(sample) if self._transform else transforms.ToTensor()(sample)
-
-        return sample, target
+        return image, target
 
 
